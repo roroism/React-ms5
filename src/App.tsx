b5ada9625438ac71bdad2c7370067c3845813d9d
roroism <miroiro0303@gmail.com>
2022-09-18 21:41:08 +0900

    feat: Create Header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Routes/Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
-
-
   return (
-    null
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
