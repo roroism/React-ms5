d299462c071bf0a408f3c85b19dba620dcf85d3b
roroism <miroiro0303@gmail.com>
2022-12-01 22:36:06 +0900

    feat: Go to search url after search.

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 import { motion, useAnimation, useScroll } from "framer-motion";
-import { Link, useMatch } from "react-router-dom";
+import {
+  Link,
+  NavigateFunction,
+  useMatch,
+  useNavigate,
+} from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Nav = styled(motion.nav)`
   display: flex;
@@ -49,7 +55,7 @@ const Item = styled.li`
   }
 `;
 
-const Search = styled.span`
+const Search = styled.form`
   color: white;
   display: flex;
   align-items: center;
@@ -105,6 +111,10 @@ const navVariants = {
   },
 };
 
+interface IForm {
+  keyword: string;
+}
+
 function Header() {
   const [searchOpen, setSearchOpen] = useState(false);
   const homeMatch = useMatch("/");
@@ -137,6 +147,13 @@ function Header() {
     });
   }, [scrollY, navAnimation]);
 
+  const navigate: NavigateFunction = useNavigate();
+  const { register, handleSubmit } = useForm<IForm>();
+  const onValid = (data: IForm) => {
+    console.log(data);
+    navigate(`/search?keyword=${data.keyword}`);
+  };
+
   return (
     <Nav variants={navVariants} animate={navAnimation} initial={"top"}>
       <Col>
@@ -163,7 +180,7 @@ function Header() {
         </Items>
       </Col>
       <Col>
-        <Search>
+        <Search onSubmit={handleSubmit(onValid)}>
           <motion.svg
             onClick={toggleSearch}
             animate={{ x: searchOpen ? -212 : 0 }}
@@ -179,6 +196,7 @@ function Header() {
             ></path>
           </motion.svg>
           <Input
+            {...register("keyword", { required: true, minLength: 2 })}
             animate={inputAnimation}
             initial={{ scaleX: 0 }}
             transition={{ type: "linear" }}
--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,5 +1,18 @@
+import { useLocation } from "react-router-dom";
+
 function Search() {
+  const location = useLocation();
+  const keyword = new URLSearchParams(location.search).get("keyword");
+  console.log(keyword);
+
   return null;
 }
 
 export default Search;
+
+// TheMovieDB Search Movies
+// API: https://api.themoviedb.org/3/search/movie?api_key=api_key&language=en-US&query=hello&page=1&include_adult=false
+// https://developers.themoviedb.org/3/search/search-movies
+
+// TheMovieDB Search TV Shows
+// https://developers.themoviedb.org/3/search/search-tv-shows
