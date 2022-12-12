fe838a1e89c96c08b9af0b00efb7bd250e3c6b64
roroism <miroiro0303@gmail.com>
2022-12-12 22:42:34 +0900

    refactor: component separation.

--- src/Components/ProgramList.tsx
+++ src/Components/ProgramList.tsx
@@ -1,68 +1,14 @@
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { motion, AnimatePresence, useScroll } from "framer-motion";
-import { getMovies, IGetMoviesResult } from "../api";
+import { getMovies, getTvshows, IGetMoviesResult, IGetTvResult } from "../api";
 import { makeImagePath } from "../utils";
 import { useState } from "react";
 import useWindowDimensions from "../Hooks/useWindowDimensions";
 import { useMatch, useNavigate } from "react-router-dom";
 import DetailInfo from "../Components/DetailMovieInfo";
-import ProgramList from "../Components/ProgramList";
+import { takeCoverage } from "v8";
 
-const Wrapper = styled.div`
-  background: black;
-  overflow-x: hidden;
-  padding-bottom: 200px;
-`;
-
-export const Loader = styled.div`
-  height: 20vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Banner = styled.div<{ bgPhoto: string }>`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: 60px;
-  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
-    url(${(props) => props.bgPhoto});
-  background-size: cover;
-`;
-
-const MovieList = styled.div`
-  position: relative;
-  top: -100px;
-`;
-
-const TvList = styled.div``;
-
-const HiddenEl = styled.h2`
-  display: block;
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-  clip-path: polygon(0 0, 0 0, 0 0);
-`;
-
-const HiddenH2 = HiddenEl.withComponent("h2");
-
-const H2 = styled.h2`
-  color: ${(props) => props.theme.white.lighter};
-  font-size: 24px;
-  padding: 7px 16px;
-  font-weight: 700;
-`;
-
-const Title = styled.h3`
-  font-size: 68px;
-  margin-bottom: 20px;
-`;
 
 const Overview = styled.p`
   font-size: 36px;
@@ -111,12 +57,6 @@ const Slider = styled.div`
   }
 `;
 
-const RowWrap = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 200px;
-`;
-
 const Row = styled(motion.div)`
   display: grid;
   gap: 5px;
@@ -140,60 +80,6 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
   &:last-child {
     transform-origin: center right;
   }
-`;
-
-const anchorEl = styled.a`
-  display: block;
-  height: 100%;
-`;
-
-const Overlay = styled(motion.div)`
-  position: fixed;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  opacity: 0;
-`;
-
-const BigMovie = styled(motion.div)<{ scrolly: number }>`
-  position: absolute;
-  width: 40vw;
-  height: 80vh;
-  top: ${(props) => props.scrolly + 100}px;
-  left: 0;
-  right: 0;
-  margin: 0 auto;
-  border-radius: 15px;
-  overflow: hidden;
-  background-color: ${(props) => props.theme.black.lighter};
-`;
-
-const BigCover = styled.div`
-  width: 100%;
-  background-size: cover;
-  background-position: center center;
-  height: 400px;
-`;
-
-const BigTitle = styled.h3`
-  color: ${(props) => props.theme.white.lighter};
-  padding: 10px;
-  /* text-align: center; */
-  font-size: 46px;
-  position: absolute;
-  width: 100%;
-  top: 330px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`;
-
-const BigOverview = styled.p`
-  padding: 20px;
-  color: ${(props) => props.theme.white.lighter};
-  position: relative;
-  top: -70px;
 `;
 
 const rowVariants = {
@@ -253,20 +139,16 @@ const infoVariants = {
 
 const offset = 6;
 
-// detail 정보 가져오기 api : GET /movie/{movie_id}
-function Home() {
+function ProgramList() {
   const navigate = useNavigate();
-  const bigMoviematch = useMatch("/movies/:movieId");
-  const { scrollY } = useScroll();
-  const { data, isLoading } = useQuery<IGetMoviesResult>(
-    ["movies", "nowPlaying"],
-    getMovies
+  const { data, isLoading } = useQuery<IGetTvResult>(
+    ["tv", "onTheAir"],
+    getTvshows
   );
-  console.log("movies data : ", data);
-  // console.log(data, isLoading);
   const [index, setIndex] = useState(0);
   const [leaving, setLeaving] = useState(false);
   const [clickedNextBtn, setClickedNextBtn] = useState(false);
+
   const width = useWindowDimensions();
   const increaseIndex = () => {
     if (data) {
@@ -292,27 +174,10 @@ function Home() {
   const onBoxClicked = (movieId: number) => {
     navigate(`/movies/${movieId}`);
   };
-  const onOverlayClick = () => navigate("/");
-  const clickedMovie =
-    bigMoviematch?.params.movieId &&
-    data?.results.find(
-      (movie) => String(movie.id) === bigMoviematch.params.movieId
-    );
-
+  
   return (
-    <Wrapper>
-      {isLoading ? (
-        <Loader>Loading</Loader>
-      ) : (
-        <>
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
-            <HiddenH2>MainVisual</HiddenH2>
-            <Title>{data?.results[0].title}</Title>
-            <Overview>{data?.results[0].overview}</Overview>
-          </Banner>
-          <MovieList>
-            <H2>on Movie</H2>
-            <Slider>
+
+    <Slider>
               <AnimatePresence
                 initial={false}
                 onExitComplete={toggleLeaving}
@@ -341,19 +206,19 @@ function Home() {
                   {data?.results
                     .slice(1)
                     .slice(offset * index, offset * index + offset)
-                    .map((movie) => (
+                    .map((tv) => (
                       <Box
-                        layoutId={movie.id + ""}
-                        key={movie.id}
+                        layoutId={tv.id + ""}
+                        key={tv.id}
                         variants={BoxVariants}
                         whileHover="hover"
                         initial="normal"
                         transition={{ type: "tween" }}
-                        onClick={() => onBoxClicked(movie.id)}
-                        bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                        onClick={() => onBoxClicked(tv.id)}
+                        bgPhoto={makeImagePath(tv.backdrop_path, "w500")}
                       >
                         <Info variants={infoVariants}>
-                          <h4>{movie.title}</h4>
+                          <h4>{tv.name}</h4>
                         </Info>
                       </Box>
                     ))}
@@ -364,48 +229,7 @@ function Home() {
                 &gt;
               </SliderRightButton>
             </Slider>
-          </MovieList>
-          <TvList>
-            <H2>Tv Shows</H2>
-            <ProgramList />
-          </TvList>
-          <AnimatePresence>
-            {bigMoviematch ? (
-              <>
-                <Overlay
-                  onClick={onOverlayClick}
-                  exit={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                />
-                <BigMovie
-                  layoutId={bigMoviematch.params.movieId}
-                  scrolly={scrollY.get()}
-                >
-                  {clickedMovie && (
-                    <>
-                      <BigCover
-                        style={{
-                          backgroundImage: `linear-gradient(to top, black, transparent), url(${makeImagePath(
-                            clickedMovie.backdrop_path,
-                            "w500"
-                          )})`,
-                        }}
-                      />
-                      <BigTitle>{clickedMovie.title}</BigTitle>
-                      {/* <BigOverview>{clickedMovie.overview}</BigOverview> */}
-                      {bigMoviematch?.params.movieId ? (
-                        <DetailInfo movieId={bigMoviematch?.params.movieId} />
-                      ) : null}
-                    </>
-                  )}
-                </BigMovie>
-              </>
-            ) : null}
-          </AnimatePresence>
-        </>
-      )}
-    </Wrapper>
   );
 }
 
-export default Home;
+export default ProgramList;
